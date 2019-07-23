3f3bb713f03ca038c4ff673a50b418356a9f603e
xiaoqiang Shao <xiaoqiang.shao@gmail.com>
2019-07-22 22:17:15 -0400

    Second Deploy

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__) #__name__ is the module will be executed
 
 @app.route("/")  #function decorator
 def home():
-    return "Hello Flask"
+    return "企鹅教育-Elise"
 
-@app.route("/test")
-def test():
-    return "this is Test"
+@app.route("/piano")
+def piano():
+    return "this is Piano"
 
 if __name__=="__main__":
     app.run() #start server
