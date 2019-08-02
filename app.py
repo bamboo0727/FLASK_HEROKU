8ef2a4ea4c1e218b9909fd143515f4512bb017b5
xiaoqiang Shao <xiaoqiang.shao@gmail.com>
2019-08-02 12:30:41 -0400

    Revert "Second Deploy"
    This reverts commit 3f3bb713

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Hello world')
+from flask import Flask
+app = Flask(__name__) #__name__ is the module will be executed
+
+@app.route("/")  #function decorator
+def home():
+    return "Hello Flask"
+
+@app.route("/test")
+def test():
+    return "this is Test"
+
+if __name__=="__main__":
+    app.run() #start server
