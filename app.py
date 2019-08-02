fd2f793068110e62ac2a0d989e67ebeb29b73da7
xiaoqiang Shao <xiaoqiang.shao@gmail.com>
2019-08-02 12:18:26 -0400

    change aug 2nd

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask
-app = Flask(__name__) #__name__ is the module will be executed
-
-@app.route("/")  #function decorator
-def home():
-    return "企鹅教育-Elise"
-
-@app.route("/piano")
-def piano():
-    return "this is Piano"
-
-if __name__=="__main__":
-    app.run() #start server
+print('Hello world')
